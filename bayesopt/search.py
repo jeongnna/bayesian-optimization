424ebd1586156c3788f93b5b43d1916435425a4c
Jeongmin Kim <lulumin13@gmail.com>
2019-10-17 05:55:16 +0000

    Update and rename hyperopt/search.py to bayesopt/search.py

--- bayesopt/search.py
+++ bayesopt/search.py
@@ -2,22 +2,19 @@ from pickle import dump
 from collections.abc import Mapping, Iterable
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
 from sklearn.model_selection._search import BaseSearchCV, ParameterSampler
 
-from .acquisition import _pick_maximum, expected_improvement
-from .utils import dist_to_bound, _sort_params, _params_to_2darray, _acq_pick
+from .acquisition import expected_improvement, pick_argmax_acq
+from .utils import dist_to_bound, _sort_params, _params_to_2darray
 
 
 class ParameterAcquirer:
 
-    def __init__(self, surrogate_model, param_distributions,
-                 n_candidates, acq_func, acq_kwargs={},
-                 n_pick=1, pick_func=_pick_maximum, pick_kwargs={},
-                 random_state=None):
+    def __init__(self, surrogate, param_distributions,
+                 acq_kwargs={}, random_state=None):
         if not isinstance(param_distributions, (Mapping, Iterable)):
             raise TypeError('Parameter distribution is not a dict or '
                             'a list ({!r})'.format(param_distributions))
@@ -33,32 +30,32 @@ class ParameterAcquirer:
                                 'or distribution (key={!r}, value={!r})'
                                 .format(key, dist[key]))
 
-        self.surrogate_model = surrogate_model
+        self.surrogate = surrogate
         self.param_distributions = param_distributions
-        self.n_candidates = n_candidates
-        self.acq_func = acq_func
         self.acq_kwargs = acq_kwargs
-        self.n_pick = n_pick
-        self.pick_func = pick_func
-        self.pick_kwargs = pick_kwargs
         self.random_state = random_state
+
+        self.pick_func = acq_kwargs.get('pick_func')
 
     def __iter__(self):
         sampler = ParameterSampler(self.param_distributions,
-                                   self.n_candidates,
+                                   self.acq_kwargs['n_candidates'],
                                    self.random_state)
         candidates = list(sampler)
         X_candidate = _params_to_2darray(candidates)
 
-        idx = _acq_pick(self.surrogate_model, X_candidate,
-                        self.acq_func, self.pick_func, self.n_pick,
-                        self.acq_kwargs, self.pick_kwargs)
+        #idx = _acq_pick(self.surrogate, X_candidate,
+        #                self.acq_func, self.pick_func, self.n_pick,
+        #                self.acq_kwargs, self.pick_kwargs)
+        idx = self.pick_func(self.surrogate,
+                               X_candidate,
+                               self.acq_kwargs)
 
         for i in idx:
             yield candidates[i]
 
     def __len__(self):
-        return self.n_pick
+        return self.acq_kwargs['n_pick']
 
 
 class EnhancedSearchCV(BaseSearchCV):
@@ -134,27 +131,18 @@ class BayesianSearchCV(EnhancedSearchCV):
                  cv=None, verbose=0, pre_dispatch='2*n_jobs',
                  random_state=None, error_score=np.nan,
                  return_train_score=False, file=None,
-                 greater_is_better=False, n_initial_points=10,
-                 n_candidates=1000, acq_func='ei', acq_kwargs={},
-                 n_pick=1, pick_func=_pick_maximum, pick_kwargs={},
+                 n_initial_points=10, acq_kwargs={},
                  gp_params={}):
-        if acq_func == 'ei':
-            acq_func = expected_improvement
-
-        acq_kwargs['greater_is_better'] = greater_is_better
 
         self.param_distributions = param_distributions
-        self.greater_is_better = greater_is_better
         self.n_initial_points = n_initial_points
         self._n_initial_points = n_initial_points
 
-        self.n_candidates = n_candidates
-        self.acq_func = acq_func
         self.acq_kwargs = acq_kwargs
-
-        self.n_pick=n_pick
-        self.pick_func=pick_func
-        self.pick_kwargs=pick_kwargs
+        self.acq_kwargs.setdefault('acq_func', expected_improvement)
+        self.acq_kwargs.setdefault('pick_func', pick_argmax_acq)
+        self.acq_kwargs.setdefault('n_candidates', 1000)
+        self.acq_kwargs.setdefault('n_pick', 1)
 
         self.gp_params = gp_params
 
@@ -186,14 +174,9 @@ class BayesianSearchCV(EnhancedSearchCV):
             gp = self._gp_fit(X_obs, y_obs)
 
             results = evaluate_candidates(ParameterAcquirer(
-                surrogate_model=gp,
+                surrogate=gp,
                 param_distributions=self.param_distributions,
-                n_candidates=self.n_candidates,
-                acq_func=self.acq_func,
                 acq_kwargs=self.acq_kwargs,
-                n_pick=self.n_pick,
-                pick_func=self.pick_func,
-                pick_kwargs=self.pick_kwargs,
                 random_state=self.random_state))
 
             self._save_cv_results(results, self.file)
@@ -201,77 +184,28 @@ class BayesianSearchCV(EnhancedSearchCV):
     def _Xy_observations(self, cv_results):
         X_obs = self._X_init.copy()
         y_obs = self._y_init.copy()
+
         if cv_results:
             X_obs.extend(cv_results['params'])
             y_obs.extend(cv_results['mean_test_score'])
+
         X_obs = _params_to_2darray(_sort_params(X_obs))
         y_obs = np.array(y_obs)
+
         return (X_obs, y_obs)
 
-    def _gp_fit(self, X, y):
-        matern = Matern(nu=2.5)
-        gp = GaussianProcessRegressor(kernel=matern,
+    def _gp_fit(self, X, y, kernel=None):
+        if kernel is None:
+            kernel = Matern(nu=2.5)
+
+        gp = GaussianProcessRegressor(kernel=kernel,
             n_restarts_optimizer=10,
             normalize_y=True,
             random_state=self.random_state)
         gp.set_params(**self.gp_params)
         gp.fit(X, y)
+
         return gp
-
-    def vis_acquisition(self, param, step, figsize=(10, 10)):
-        """
-        Parameters
-        ----------
-
-        param : string, name of the parameter you want to visualize
-
-        step : integer
-
-        figsize (optional) : tuple of length 2
-        """
-        plt.figure(figsize=figsize)
-
-        # observations
-        X, y = self._Xy_observations(self.cv_results_)
-        X_obs = X[:step]
-        y_obs = y[:step]
-        param_pos = sorted(self.param_distributions).index(param)
-        X_obs = X_obs[:, [param_pos]]
-
-        # plot observations
-        plt.subplot(211)
-        alpha = self.gp_params.get('alpha', 1e-10)
-        plt.errorbar(X_obs.reshape(-1), y_obs, yerr=np.sqrt(alpha),
-            fmt='k.', label='Observation')
-
-        # points for drawing curve
-        param_bounds = dist_to_bound(self.param_distributions)
-        left, right = param_bounds[param]
-        xnew = np.linspace(left, right, 1000)
-
-        # derive GP & acquisition
-        gp = self._gp_fit(X_obs, y_obs)
-        mu, sigma = gp.predict(xnew.reshape(-1, 1), return_std=True)
-        mu = mu.reshape(-1)
-
-        acq = self.acq_func(gp, xnew.reshape(-1, 1), **self.acq_kwargs)
-
-        # plot GP
-        plt.subplot(211)
-        plt.plot(xnew, mu, 'C0', label='GP estimate')
-        plt.fill(np.concatenate([xnew, xnew[::-1]]),
-            np.concatenate([mu - 1.96 * sigma, (mu + 1.96 * sigma)[::-1]]),
-            alpha=0.1, fc='C0', ec='None', label='GP uncertainty')
-        plt.axvline(xnew[np.nanargmax(acq)], linestyle='--', color='C3')
-        plt.ylabel('CV score')
-        plt.legend(loc='lower right')
-
-        # plot acquisition
-        plt.subplot(212)
-        plt.plot(xnew, acq, color='C0')
-        plt.axvline(xnew[np.nanargmax(acq)], linestyle='--', color='C3')
-        plt.ylabel('Acquisition')
-        plt.xlabel(param)
 
 
 class RandomSearchCV(EnhancedSearchCV):
